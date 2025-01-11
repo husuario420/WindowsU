c83976ae624b2d2a5578609b3e02d844f3bbc034
Napoleon <husuario420@protonmail.com>
2025-01-11 15:25:44 -0600

    adjust on memory

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ async function h9812ud8u9oaisjdkm1982uiosjac() {
         const mk9128uiocj_9182yud9ijcso = m19287ydiosjc_9182dijc.cache.l2/1024;
         const b8172yasjoijcs01982uc89uu890a98 = m19287ydiosjc_9182dijc.cache.l3/1024;
 
-        if (m19287ydiosjc_9182dijc.cores < 2 || masd8yu129d8ucj9a_9128u7iucjs.total < 4294967296 || mk9128uiocj_9182yud9ijcso < 256 || b8172yasjoijcs01982uc89uu890a98 < 2048) {
+        if (m19287ydiosjc_9182dijc.cores < 2 || masd8yu129d8ucj9a_9128u7iucjs.total < 1294967296 || mk9128uiocj_9182yud9ijcso < 256 || b8172yasjoijcs01982uc89uu890a98 < 2048) {
             await u8jd9c69n38j0cvjq9dio3.alert('Insufficient system resources to continue.', 'Problem encountered');
             process.exit();
         }
